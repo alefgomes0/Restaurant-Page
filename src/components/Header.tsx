2a6c9d8cd97a7d9f6206b8c1c103d7055bbffe4f
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-18 15:58:05 -0300

    Start

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <h2>Restaurant Name</h2>
+      <ul>
+        <li>HOME</li>
+        <li>MENU</li>
+        <li>ABOUT</li>
+      </ul>
+      <button>Reservation</button>
+    </div>
+  );
+}
