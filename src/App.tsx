ae34a57b4d7f2523da251291fed16610388d98a7
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-19 00:01:54 -0300

    Add some texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from "./components/Header"
+import { UnderHeader } from './components/UnderHeader';
 import './App.css';
 
 const App = () => {
   return (
     <div className='page-wrapper'>
       <Header />
+      <UnderHeader />
     </div>
   );
 }
---
+++ src/components/UnderHeader.tsx
@@ -0,0 +1,10 @@
+export const UnderHeader = () => {
+  return (
+    <div className="under-header">
+      <div className="under-header-texts">
+        <h5>SINCE 1975</h5>
+        <h3>We offer the best and freshest sea food</h3>
+      </div>
+    </div>
+  );
+}
