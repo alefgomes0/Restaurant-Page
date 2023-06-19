9cc94f1a554dc7c1ca4c092c0bd37f4da766b76c
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-19 10:11:50 -0300

    Add more styles

--- src/components/UnderHeader.tsx
+++ src/components/UnderHeader.tsx
@@ -3,8 +3,17 @@ export const UnderHeader = () => {
     <div className="under-header">
       <div className="under-header-texts">
         <h5>SINCE 1975</h5>
-        <h3>We offer the best and freshest sea food</h3>
+        <h3>
+          We offer the <span className="blue">freshest</span> sea food
+        </h3>
+        <div className="subtext">
+          <p>
+            Pellentesque id nibh tortor id. Ultrices dui sapien eget mi proin
+            sed libero enim sed. Sagittis purus sit amet volutpat. Orci eu
+            lobortis <span className="orange">elementum</span> nibh tellus.
+          </p>
+        </div>
       </div>
     </div>
   );
-}
+};
