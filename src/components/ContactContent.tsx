a0360f7079027cfa75b90c359f085b2a706de25e
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-25 11:21:24 -0300

    Begin to add content to the "Contact" tab

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -1,3 +1,30 @@
 export const ContactContent = () => {
-  return <div className="contact-content">Contact</div>;
+  return (
+    <div className="contact-content">
+      <form>
+        <div className="contact-text">
+          <h5>Do you have any critic or suggestion?</h5>
+          <h5>Are you interested in working with us?</h5>
+          <h5>Send a message below!</h5>
+        </div>
+        <div className="field">
+          <label>Name</label>
+          <input></input>
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input type="email"></input>
+        </div>
+        <div className="field">
+          <label>Subject</label>
+          <input></input>
+        </div>
+        <div className="field">
+          <label>Message</label>
+          <textarea rows={10}></textarea>
+        </div> 
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
