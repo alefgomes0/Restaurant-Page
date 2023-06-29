f317e97371685f601b459d9613cf4dc8a3424c32
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-29 13:43:44 -0300

    Adjust some styles

--- src/components/ReasonsWhy.tsx
+++ src/components/ReasonsWhy.tsx
@@ -8,8 +8,8 @@ export const ReasonsWhy = () => {
         <div className="card card-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
+            width="36"
+            height="36"
             viewBox="0 0 24 24"
           >
             <path
@@ -31,8 +31,8 @@ export const ReasonsWhy = () => {
         <div className="card card-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
+            width="36"
+            height="36"
             viewBox="0 0 24 24"
           >
             <path
@@ -49,8 +49,8 @@ export const ReasonsWhy = () => {
         <div className="card card-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
+            width="36"
+            height="36"
             viewBox="0 0 256 256"
           >
             <path
@@ -58,7 +58,7 @@ export const ReasonsWhy = () => {
               d="M168 128a40 40 0 1 1-40-40a40 40 0 0 1 40 40Zm80-64v128a8 8 0 0 1-8 8H16a8 8 0 0 1-8-8V64a8 8 0 0 1 8-8h224a8 8 0 0 1 8 8Zm-16 46.35A56.78 56.78 0 0 1 193.65 72H62.35A56.78 56.78 0 0 1 24 110.35v35.3A56.78 56.78 0 0 1 62.35 184h131.3A56.78 56.78 0 0 1 232 145.65Z"
             />
           </svg>
-          <h3>Money</h3>
+          <h3>Price</h3>
           <h4>
             Euismod nisi porta lorem mollis aliquam ut porttitor. Mauris in
             aliquam sem fringilla ut morbi tincidunt augue. Massa sapien
