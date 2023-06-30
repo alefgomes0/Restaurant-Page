5c7eada37c04791adcc505f56d2bbcb19bbfb409
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-30 17:38:00 -0300

    Change styles

--- src/components/ReasonsWhy.tsx
+++ src/components/ReasonsWhy.tsx
@@ -2,7 +2,7 @@ export const ReasonsWhy = () => {
   return (
     <div className="reasons-why">
       <h2>
-        Why you will <span className="orange">love</span> our restaurant
+        <span className="orange">Features</span>
       </h2>
       <div className="cards-container">
         <div className="card card-1">
@@ -23,13 +23,27 @@ export const ReasonsWhy = () => {
                 d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2M7 2v20m14-7V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7"
               />
             </svg>
-            <h3>Food</h3>
+            <h3>Excellent Food</h3>
           </div>
           <h4>
             Ac auctor augue mauris augue neque gravida in. Consectetur purus ut
             faucibus pulvinar. Auctor urna nunc id cursus metus aliquam. Feugiat
             sed lectus vestibulum mattis.
           </h4>
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              viewBox="0 0 20 20"
+              className="arrow"
+            >
+              <g transform="rotate(90 10 10)">
+                <path
+                  fill="currentColor"
+                  d="M3.133 9.16a.5.5 0 1 0 .738.675l5.631-6.168v13.831a.5.5 0 1 0 1 0V3.67l5.628 6.165a.5.5 0 0 0 .739-.674l-6.314-6.916a.746.746 0 0 0-.632-.24a.746.746 0 0 0-.476.24L3.133 9.16Z"
+                />
+              </g>
+            </svg>
         </div>
         <div className="card card-2">
           <div className="idk">
@@ -37,20 +51,37 @@ export const ReasonsWhy = () => {
               xmlns="http://www.w3.org/2000/svg"
               width="36"
               height="36"
-              viewBox="0 0 24 24"
+              viewBox="0 0 14 14"
             >
               <path
-                fill="currentColor"
-                d="M20 12h2v2h-2c-1.38 0-2.74-.35-4-1c-2.5 1.3-5.5 1.3-8 0c-1.26.65-2.63 1-4 1H2v-2h2c1.39 0 2.78-.47 4-1.33c2.44 1.71 5.56 1.71 8 0c1.22.86 2.61 1.33 4 1.33m0-6h2v2h-2c-1.38 0-2.74-.35-4-1c-2.5 1.3-5.5 1.3-8 0c-1.26.65-2.63 1-4 1H2V6h2c1.39 0 2.78-.47 4-1.33c2.44 1.71 5.56 1.71 8 0C17.22 5.53 18.61 6 20 6m0 12h2v2h-2c-1.38 0-2.74-.35-4-1c-2.5 1.3-5.5 1.3-8 0c-1.26.65-2.63 1-4 1H2v-2h2c1.39 0 2.78-.47 4-1.33c2.44 1.71 5.56 1.71 8 0c1.22.86 2.61 1.33 4 1.33Z"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7 3h0a6.5 6.5 0 0 1 6.5 6.5v0a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1v0A6.5 6.5 0 0 1 7 3Zm0 0V1.5m-6.5 11h13"
               />
             </svg>
-            <h3>View</h3>
+            <h3>Great Service</h3>
           </div>
           <h4>
             Pulvinar elementum integer enim neque volutpat ac tincidunt.
             Bibendum neque egestas congue quisque egestas diam. Mollis aliquam
             ut porttitor leo a diam sollicitudin tempor.
           </h4>
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              viewBox="0 0 20 20"
+              className="arrow"
+            >
+              <g transform="rotate(90 10 10)">
+                <path
+                  fill="currentColor"
+                  d="M3.133 9.16a.5.5 0 1 0 .738.675l5.631-6.168v13.831a.5.5 0 1 0 1 0V3.67l5.628 6.165a.5.5 0 0 0 .739-.674l-6.314-6.916a.746.746 0 0 0-.632-.24a.746.746 0 0 0-.476.24L3.133 9.16Z"
+                />
+              </g>
+            </svg>
         </div>
         <div className="card card-3">
           <div className="idk">
@@ -65,12 +96,12 @@ export const ReasonsWhy = () => {
                 d="M7 15h2c0 1.08 1.37 2 3 2s3-.92 3-2c0-1.1-1.04-1.5-3.24-2.03C9.64 12.44 7 11.78 7 9c0-1.79 1.47-3.31 3.5-3.82V3h3v2.18C15.53 5.69 17 7.21 17 9h-2c0-1.08-1.37-2-3-2s-3 .92-3 2c0 1.1 1.04 1.5 3.24 2.03C14.36 11.56 17 12.22 17 15c0 1.79-1.47 3.31-3.5 3.82V21h-3v-2.18C8.47 18.31 7 16.79 7 15Z"
               />
             </svg>
-            <h3 className="price">Price</h3>
+            <h3 className="price">Affordable Prices</h3>
           </div>
           <h4>
             Euismod nisi porta lorem mollis aliquam ut porttitor. Mauris in
             aliquam sem fringilla ut morbi tincidunt augue. Massa sapien
-            faucibus et molestie ac. 
+            faucibus et molestie ac.
           </h4>
         </div>
       </div>
