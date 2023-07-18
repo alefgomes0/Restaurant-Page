88914c974fe0f11072c053e202d764244c3996ca
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-07-18 15:47:01 -0300

    Change colors

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,9 +1,9 @@
 export const SocialMediaIcons = () => {
   return (
     <div className="media-icons-container">
-      <img src="https://icongr.am/entypo/instagram-with-circle.svg?size=18&color=8c8c8c" alt="instagram icon" />
-      <img src="https://icongr.am/entypo/facebook-with-circle.svg?size=18&color=8c8c8c" alt="facebook icon" />
-      <img src="https://icongr.am/entypo/twitter-with-circle.svg?size=18&color=8c8c8c" alt="twitter icon" />
+      <img src="https://icongr.am/entypo/instagram-with-circle.svg?size=18&color=#787c87" alt="instagram icon" />
+      <img src="https://icongr.am/entypo/facebook-with-circle.svg?size=18&color=#787c87" alt="facebook icon" />
+      <img src="https://icongr.am/entypo/twitter-with-circle.svg?size=18&color=#787c87" alt="twitter icon" />
     </div>
   );
 };
