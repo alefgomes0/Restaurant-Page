cb50d436ae2eac6ef838f6882b42cbc0c003910b
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-16 11:14:30 -0300

    Change NoMatch element

--- src/Pages/NoMatch/NoMatch.tsx
+++ src/Pages/NoMatch/NoMatch.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const NoMatch = () => {
   return (
     <div className="no-match">
-      <h3>Page not found</h3>
+      <img
+        src="/images/undraw_Road_sign_re_3kc3.png"
+        alt="a man at a crossroad"
+      />
+      <h4>It seems like you are lost.</h4>
+      <Link to="/">
+        <button>Go back home</button>
+      </Link>
     </div>
   );
 };
